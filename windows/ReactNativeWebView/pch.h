4a62c2e885d2cee7434afe614acb3c6421a41c9f
Burton Rodman <burton.rodman@corebts.com>
2022-02-22 14:50:56 -0500

    building with NuGet packages

--- windows/ReactNativeWebView/pch.h
+++ windows/ReactNativeWebView/pch.h
@@ -13,9 +13,14 @@
 
 #include <winrt/Microsoft.ReactNative.h>
 
+#include <winrt/Windows.Data.Json.h>
+#include <winrt/Windows.UI.Popups.h>
 #include <winrt/Microsoft.UI.Xaml.Automation.Peers.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
 #include <winrt/Microsoft.UI.Xaml.Controls.h>
 #include <winrt/Microsoft.UI.Xaml.Media.h>
 #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
+#include <winrt/Windows.Web.Http.h>
+#include <winrt/Windows.Web.Http.Headers.h>
+
 using namespace winrt::Windows::Foundation;
